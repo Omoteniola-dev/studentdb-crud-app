bb500440fd7ff60bfe0047bc3f55e8a70e48afeb
Teniola <fatunmbiteniola001@gmail.com>
2021-06-07 14:54:48 +0100

    added authentication to to student routes

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,29 +1,30 @@
 const express = require("express")
 const router = express.Router()
 const { welcome, fetchStudents, fetchStudentsByID, createNewStudent, deleteAllStudents, deleteStudentById, updateStudentById } = require("../controllers/dbControllers")
+const { authenticateUser } = require("../middlewares/authentication");
 
-// Welcome message
-router.get("/", welcome)
+// Welcome messageg
+router.get("/", welcome);
 
 //GET request to fetch the students in the database.
-router.get("/students", fetchStudents)
+router.get("/students", authenticateUser, fetchStudents);
 
 // GET request to fetch student by id
-router.get("/students/:id", fetchStudentsByID)
+router.get("/students/:id", authenticateUser, fetchStudentsByID);
 
 //POST request to add a new student.
-router.post("/students", createNewStudent)
+router.post("/students", createNewStudent);
 
 
 // DELETE request to clear the database
-router.delete("/students", deleteAllStudents)
+router.delete("/students", authenticateUser, deleteAllStudents);
 
 
 //DELETE request to delete student by id
-router.delete("/students/:id", deleteStudentById)
+router.delete("/students/:id", authenticateUser, deleteStudentById);
 
 // PUT request to update the data created by ID
-router.put("/students/:id", updateStudentById)
+router.put("/students/:id", authenticateUser, updateStudentById);
 
 module.exports = router;
 
