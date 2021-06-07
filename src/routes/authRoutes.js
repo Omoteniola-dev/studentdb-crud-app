3b54df18aecc4b1b801ebd5e01f98d9655941b88
Teniola <fatunmbiteniola001@gmail.com>
2021-06-07 14:54:58 +0100

    added authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { createNewUser } = require("../controllers/authControllers");
+const { createNewUser, loginUser } = require("../controllers/authControllers");
 
 router.post("/signup", createNewUser);
-
+router.post("/login", loginUser);
 module.exports = router;
