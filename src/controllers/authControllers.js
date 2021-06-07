9a72520d7fec472be5abc3e17d5fc64704963d7d
Teniola <fatunmbiteniola001@gmail.com>
2021-06-07 14:51:19 +0100

    added authControllers

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,16 +1,19 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const secret = "Asecret";
-const expiry = 4800;
+const secret = process.env.JWT_SECRET;
+const expiry = process.env.EXPIRY;
 
 // create a new user
 exports.createNewUser = (req, res) =>{
+    // Check if a user with the username exist
     User.findOne({username: req.body.username}, (err, existingUser) => {
         if (err) return res.status(500).json({ message: err })
-        if (existingUser) return res.status(400).json({ message: "User already exists."})
+        if (existingUser) return res.status(400).json({ message: "User already exists."});
+
     })
-    
+
+    //create a new user
     User.create({
         ...req.body
     }, (err, newUser) => {
@@ -25,8 +28,8 @@ exports.createNewUser = (req, res) =>{
                     newUser.password = hashedPassword;
                     newUser.save((err, savedUser) => {
                         if (err) return res.status(500).json({ message: err});
-                        // create JWT for savedUser.
                         
+                        // create JWT for savedUser.
                         jwt.sign({
                             firstname: savedUser.firstname,
                             lastname: savedUser.lastname,
@@ -52,5 +55,28 @@ exports.createNewUser = (req, res) =>{
 // login a user
 exports.loginUser = (req, res) => {
     // check if the provided username and password matches any on the database
-    // grant access to user if it does
-}
+    User.findOne({ username: req.body.username}, (err, foundUser) => {
+        if(err) return res.status(500).json({ message: err});
+        if(!foundUser) return res.status(401).json({ message: "Incorrect username"});
+
+        console.log(foundUser);
+        // Check if the password is correct with the one in the database.
+        let match = bcrypt.compare(req.body.password, foundUser.password);
+        if(!match) return res.status(500).json({ message: "Incorrect password"});
+
+        // create JWT for foundUser.
+        jwt.sign({
+            firstname: foundUser.firstname,
+            lastname: foundUser.lastname,
+            username: foundUser.username
+        },
+        secret, {expiresIn: expiry}, (err, token) => {
+            if (err) return res.status(500).json({ message: err });
+
+            //send token to user
+            if(!token) return res.status(500).json({ message: err })
+            else return res.status(200).json({ message: "login successful", token})
+        });
+        
+    });
+};
