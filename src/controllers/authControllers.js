93ddba9f62485f8178af3179b40bb5855d5553f4
Teniola <fatunmbiteniola001@gmail.com>
2021-06-12 17:09:15 +0100

    fixed duplication bug in database

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -5,51 +5,42 @@ const secret = process.env.JWT_SECRET;
 const expiry = process.env.EXPIRY;
 
 // create a new user
-exports.createNewUser = (req, res) =>{
+exports.createNewUser = async (req, res) =>{
     // Check if a user with the username exist
-    User.findOne({username: req.body.username}, (err, existingUser) => {
-        if (err) return res.status(500).json({ message: err })
-        if (existingUser) return res.status(400).json({ message: "User already exists."});
+    try{
+        const existingUser = await User.findOne({username: req.body.username});
+        if(existingUser) {
+            return res.status(400).json({ message: "User already exists"});
+        }
 
-    })
+    }
+    catch(err){
+        return res.status(500).json({ message: err });
+    }
 
     //create a new user
-    User.create({
-        ...req.body
-    }, (err, newUser) => {
-        if (err) return res.status(500).json({ message: err });
-        else{
-            //Salt and hash the user's password
-            bcrypt.genSalt(10, (err, salt) => {
-                if (err) return res.status(500).json({ message: err});
-                bcrypt.hash(req.body.password, salt, (err,hashedPassword) => {
-                    if (err) return res.status(500).json({ message: err});
-                    // save hashed password to database
-                    newUser.password = hashedPassword;
-                    newUser.save((err, savedUser) => {
-                        if (err) return res.status(500).json({ message: err});
-                        
-                        // create JWT for savedUser.
-                        jwt.sign({
-                            firstname: savedUser.firstname,
-                            lastname: savedUser.lastname,
-                            username: savedUser.username
-                        },
-                        secret, {expiresIn: expiry}, (err, token) => {
-                            if (err) return res.status(500).json({ message: err });
+    try{
+        const newUser = await User.create({ ...req.body });
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        newUser.password = hashedPassword;
+        const savedUser = await newUser.save();
+        // Create a JWT
+        const token = await jwt.sign({ 
+            firstname: savedUser.firstname,
+            lastname: savedUser.lastname,
+            username: savedUser.username
+        },
+        secret, {expiresIn: expiry});
 
-                            //send token to user
-                            if(!token) return res.status(500).json({ message: "Sorry, we could not authenticate you. Please login"})
-                            else return res.status(200).json({ message: "user registration successful", token})
-                        })
-                    
-                        
-                        
-                    })
-                })
-            })
-        }
-    })
+        //send token to user
+        if(!token) return res.status(500).json({ message: "Sorry, we could not authenticate you. Please login"});
+        else return res.status(200).json({ message: "user registration successful", token});
+
+    }
+    catch(err){
+        throw err
+    }
 }
 
 // login a user
