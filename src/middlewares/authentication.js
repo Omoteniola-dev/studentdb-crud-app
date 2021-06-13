8cf262c29016ca4bdbad09f6ca65487e087d1a65
Teniola <fatunmbiteniola001@gmail.com>
2021-06-13 16:45:09 +0100

    code refactoring

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const { decodeToken } = require("../services/jwtService");
 const secret = process.env.JWT_SECRET;
 
 exports.authenticateUser = (req, res, next) => {
@@ -13,12 +14,15 @@ exports.authenticateUser = (req, res, next) => {
     if(splittedHeaders[0] !== "Bearer") return res.status(500).json({ message: "Authorization header must be in the format Bearer <token>"})
     
     let token = splittedHeaders[1];
-    
+
     //decode the token
-    jwt.verify(token, secret, (err, decodedToken) => {
-        if(err) return res.status(500).json({ message: err });
-        //check if valid
-        if (!token) return res.status(401).json({ message: "Invalid authorization token. Please login."})
-        else return decodedToken;
-    });
+    let decodedToken = decodeToken(token);
+    
+    //check if valid
+    if (!token) return res.status(401).json({ message: "Invalid authorization token. Please login."})
+    else return decodedToken;
+}
+
+exports.checkIfAdmin = (req, res, next) => {
+    
 }
